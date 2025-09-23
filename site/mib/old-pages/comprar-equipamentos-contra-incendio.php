a4a3ad33e629ab3b0c238af316413c12a3dea7b6
bisslee <bisslee@gmail.com>
2025-09-23 19:40:16 -0300

    Ajsutando imagens

--- site/mib/old-pages/comprar-equipamentos-contra-incendio.php
+++ site/mib/old-pages/comprar-equipamentos-contra-incendio.php
@@ -81,7 +81,7 @@ if (isset($breadcrumbs)) {
 
  <div>
 
- <img src="/assets/img/old-site/planilha-60.png" alt="">
+ <img src="/assets/img/old-site/planilha-60.png" class="img-fluid" alt="">
 
  </div>
 
--- site/mib/old-pages/esguicho-regulavel-em-aluminio.php
+++ site/mib/old-pages/esguicho-regulavel-em-aluminio.php
@@ -81,7 +81,7 @@ if (isset($breadcrumbs)) {
 
  <div>
 
- <img src="/assets/img/old-site/planilha-61.png" alt="">
+ <img src="/assets/img/old-site/planilha-61.png" class="img-fluid" alt="">
 
  </div>
 
